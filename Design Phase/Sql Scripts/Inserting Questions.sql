34fe8715213cf76f834e20c271a3458ef271fe65
EddTally <32542536+EddTally@users.noreply.github.com>
2020-02-25 12:36:33 +0000

    Add files via upload

---
+++ Design Phase/Sql Scripts/Inserting Questions.sql
@@ -0,0 +1,3 @@
+INSERT INTO biologyquestions (questionNumber, questionText, questionAnswer, questionAttempted, multipleChoiceAnswers, biology_idbiology)
+VALUES (1, "Question?", "Answer", false, "No-Answer-No", 5);
+SELECT * FROM scienceforkids.biologyquestions;
---
+++ Design Phase/Sql Scripts/Inserting content into topics.sql
@@ -0,0 +1,3 @@
+INSERT INTO chemistry (topicYear, topicName, topicContents, questionnaireScore, completedTopic, topicImg) 
+VALUES (3, "alkali", "this is the topic content for the alkaline topic", 0, false, NULL);
+SELECT * FROM scienceforkids.chemistry;
